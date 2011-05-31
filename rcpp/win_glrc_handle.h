18c0806ed73c9c6a8abaac3ab77af424f317c24e
Ivan Sorokin <sorokin@rain.ifmo.ru>
2011-06-01 01:27:54 +0400

    push year up

--- rcpp/win_glrc_handle.h
+++ rcpp/win_glrc_handle.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010 Ivan Sorokin
+Copyright (c) 2010, 2011 Ivan Sorokin
 
 Distributed under the Boost Software License, Version 1.0. (See
 accompanying file LICENSE_1_0.txt or copy at
